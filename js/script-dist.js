340ec5c2fccea95577df7e29c5d45c2d2561482c
Vilius <v.siuksteris@adfingers.com>
2017-09-29 14:17:23 +0300

    Mobile appearance image margin and footer button position fixed. Added animation to sidebar icons.

---
+++ js/script-dist.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$("#arrow-down").click(function(){$(".clearfix").toggleClass("d-none")}),$(".clearfix li").hover(function(){$(this).css("margin-left","-6px")},function(){$(this).css("margin-left","-12px")})});
--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@ $(document).ready(function() {
 
 	$("#arrow-down").click(function() {
 		$(".clearfix").toggleClass("d-none");
+	});
+
+	$(".clearfix li").hover(function() {
+		$(this).css("margin-left", "-6px");
+	}, function() {
+		$(this).css("margin-left", "-12px");
 	})
 })
